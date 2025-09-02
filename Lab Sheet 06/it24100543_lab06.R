584c27dea978b2722f2b50fc328cd30e2b1f608f
IT24100543 <it24100543@my.sliit.lk>
2025-09-02 16:34:10 +0530

    Add files via upload

---
+++ Lab Sheet 06/it24100543_lab06.R
@@ -0,0 +1,11 @@
+setwd("C:\\Users\\it24100543\\Desktop\\It24100543_lab06")
+
+n=50
+p=0.85
+
+#What is the probability that at least 47 students passed the test?
+dbinom(47,50,0.85)
+
+#2
+#What is the probability that exactly 15 calls are received in an hour?
+dpois(15,12)
