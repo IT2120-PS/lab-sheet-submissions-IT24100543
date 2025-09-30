64e73203a2ee348e0a688aa7ac983f2ae2d8fa89
IT24100543 <it24100543@my.sliit.lk>
2025-09-30 21:38:13 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24100543_PS_Lab_09.R
@@ -0,0 +1,7 @@
+#IT24100543 Kulasingha A.I.V.
+
+#  i. Generate a random sample of size 25 for the baking time.
+data <- rnorm(25, mean = 45, sd = 2)
+
+# ii. Test whether the average baking time is less than 46 minutes at a 5% level of significance.
+t.test(data, mu = 46, alternative = "less")
